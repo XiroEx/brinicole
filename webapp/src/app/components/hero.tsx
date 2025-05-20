b8bf6efdf6c4b16382cbc499d5febb079b0a783f
George Abreu Jr. <george8794@gmail.com>
2025-05-20 17:00:32 +0000

    gallery photos uploaded, new profile

--- webapp/src/app/components/hero.tsx
+++ webapp/src/app/components/hero.tsx
@@ -64,19 +64,19 @@ export default function Hero() {
             >
                 <div className={`${hovered || active ? '' : 'hidden'} grid grid-cols-1 md:grid-cols-3 gap-4 max-w-6xl mx-auto`}>
                     {/* Pictures! */}
-                    <Image src="/brinicole.jpg" alt="Bri Nicole" width={500} height={500} className="rounded-4xl mt-4 mb-4 pop-huge mx-auto" />
+
+                    {Array.from({ length: 15 }, (_, i) => (
+                        <Image
+                            key={i + 1}
+                            src={`/photos/Bri${i + 1}.JPG`}
+                            alt="Bri Nicole"
+                            width={500}
+                            height={500}
+                            className="rounded-4xl mt-4 mb-4 pop-huge mx-auto"
+                        />
+                    ))}
                     
-                    <Image src="/brinicole2.jpg" alt="Bri Nicole" width={500} height={500} className="rounded-4xl mt-4 mb-4 pop-huge mx-auto" />
-                    
-                    <Image src="/brinicole.jpg" alt="Bri Nicole" width={500} height={500} className="rounded-4xl mt-4 mb-4 pop-huge mx-auto" />
-                    
-                </div>
-                <p className={`text-xl text-[var(--color-accent)] max-w-6xl mx-auto mb-6 md:mb-0 pop-big ${hovered || active ? '' : 'hidden'}`}>
-                    Bri is an emerging R&B artist with roots deeply embedded in the heart of Long Island, New York. Born and raised in a close-knit suburban environment, she found her early musical inspiration right at home with her parents and brother. From a tender age, Bri was drawn to the captivating melodies and heartfelt lyrics of R&B classics, a genre that would go on to define her own musical identity. Influenced by contemporary R&B luminaries like H.E.R, SZA, Summer Walker, and Kiana Lede, her music beautifully blends the soulful essence of the old-school R&B she grew up loving with a fresh, modern twist.
-                    <br />
-                    <br />
-                    Bri’s music is an emotional journey that resonates with listeners on a profound level. Her soulful sound is the conduit through which she explores universal themes of love, heartbreak, and the sheer pleasure of living life to the fullest. Her songs are more than just melodies; they are relatable stories that touch the heart and speak to the soul. Through her music, she aims to create a genuine connection with her audience, allowing them to not only hear but feel the emotions she pours into every note and lyric.
-                </p>
+                </div> 
             </div>
             <span
                 ref={heroRef}
